f30cde5b8fc9b7581a5d618ff617396d1424ee52
Dansin <ulysse.dansin@rakuten.com>
2021-10-19 10:13:59 +0200

    Create mosaic

--- app/services/ffmpeg.rb
+++ app/services/ffmpeg.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-require 'fileutils'
-require 'securerandom'
-require 'byebug'
-
 module Services
   class Ffmpeg
-    attr_reader :file_path, :output_path, :opts
+    attr_reader :file_path, :media_info, :output_path, :opts
 
     ACCEPTED_FORMATS = %w(.mp4).freeze
+    IMAGE_PER_SEC = 5
+    NB_ROWS = 15
+    NB_COLUMNS = 15
 
     def initialize(file_path, opts = {})
       @file_path = file_path
+      @media_info = Services::Media::Info.new(file_path)
       @opts = opts
       @output_path = setup_output_dir
     end
 
     def call
       check_input
-      create_mosaic
+      create_mosaics
     end
 
     private
@@ -33,14 +33,34 @@ module Services
       FileUtils.mkdir_p("/tmp/mosaic/#{SecureRandom.hex(10)}")
     end
 
-    def create_mosaic
-      system command.to_s
+    def create_mosaics
+      total_duration_per_mosaic, nb_mosaic_needed = mosaic_values
+      nb_proceed_mosaic = 0
+      ss = 0
+      t = total_duration_per_mosaic
+
+      while nb_proceed_mosaic != nb_mosaic_needed
+        t = t > media_info.duration ? media_info.duration : t
+
+        system command(ss, t).to_s
+        nb_proceed_mosaic += 1
+        ss = t
+        t += total_duration_per_mosaic
+      end
     end
 
-    def command
-      "ffmpeg -y -i #{file_path} -filter_complex \"select='not(mod(n,120))'," \
-      'scale=128:72,tile=11x11" -frames:v 1 -qscale:v 3 ' \
-      "-an #{output_path.first}/mosaic.jpg"
+    def mosaic_values
+      total_duration_per_mosaic = NB_COLUMNS * NB_ROWS * IMAGE_PER_SEC
+      nb_mosaic_needed = (media_info.duration / total_duration_per_mosaic).ceil
+      [total_duration_per_mosaic, nb_mosaic_needed]
+    end
+
+    def command(start_time, trim)
+      "ffmpeg -y -skip_frame nokey -i #{file_path} -vf \"select=" \
+      "'between(t,#{start_time},#{trim})'," \
+      "setpts=#{media_info.framerate * IMAGE_PER_SEC}," \
+      "scale=128:72,tile=#{NB_COLUMNS}x#{NB_ROWS}\" -frames:v 1 -qscale:v 3 " \
+      "-an #{output_path.first}/mosaic_#{start_time}.jpg"
     end
   end
 end
